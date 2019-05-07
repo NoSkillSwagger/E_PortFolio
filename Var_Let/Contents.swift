f7493d253bc9aa61ce05f76c5a48b5f2c93b0744
NoSkillSwagger <40427797+NoSkillSwagger@users.noreply.github.com>
2019-05-07 20:30:51 +0200

    added Var_Let playground

---
+++ Var_Let/Contents.swift
@@ -0,0 +1,12 @@
+var myString = "Hello"
+var myInt = 10
+
+print(myString)
+print(myInt)
+
+var myString2: String 
+var myInt2: Int
+
+let myConstantVariable = "Constant"
+
+//myConstantVariable = "changed" // not possible
